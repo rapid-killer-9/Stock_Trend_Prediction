f2adcb37aba577902089438dbac1c50404a9ca8a
Harsh <hjha0695@gmail.com>
2023-04-04 00:22:37 +0530

    changed module pandas-datareader to yfinance coz pandas-datareader having some sort of error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 from datetime import date
 from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
-import pandas_datareader as data
+import yfinance as yf
 from keras.models import load_model
 import streamlit as st
 
@@ -41,40 +41,40 @@ date_range = st.selectbox("Select Time Period", options= date_range)
 selected = date_range
 start = past_date(selected)
 end = date.today()
-df = data.DataReader(user_input,'yahoo',start ,end)
+data = yf.download(user_input, start=start, end=end)
 
 #Describing Data
 st.write('Data Form ',start,'-',end)
-st.write(df.describe())
+st.write(data.describe())
 
 #Data Visualizations
 st.subheader('Closing Price vs Time-Chart')
 fig = plt.figure(figsize=(12,6))
-plt.plot(df.Close )
+plt.plot(data.Close )
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time-Chart with 100MA')
-ma100 = df.Close.rolling(100).mean()
+ma100 = data.Close.rolling(100).mean()
 fig = plt.figure(figsize=(12,6))
 plt.plot(ma100,'r',label='MA100')
-plt.plot(df.Close,'b',label='Closing')
+plt.plot(data.Close,'b',label='Closing')
 plt.legend()
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time-Chart with 100MA & 200MA')
-ma100 = df.Close.rolling(100).mean()
-ma200 = df.Close.rolling(200).mean()
+ma100 = data.Close.rolling(100).mean()
+ma200 = data.Close.rolling(200).mean()
 fig = plt.figure(figsize=(12,6))
 plt.plot(ma100,'r',label='MA100')
 plt.plot(ma200,'g',label='MA200')
-plt.plot(df.Close,'b',label='Closing')
+plt.plot(data.Close,'b',label='Closing')
 plt.legend()
 st.pyplot(fig)
 
 #Splitting Data Into Training and Testing
 
-data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+data_training = pd.DataFrame(data['Close'][0:int(len(data)*0.70)])
+data_testing = pd.DataFrame(data['Close'][int(len(data)*0.70): int(len(data))])
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -86,8 +86,8 @@ model=load_model('Keras_Model.h5')
 
 # Testing the model for the starting 100 days from the selected range
 last_100_days = data_training.tail(100)
-final_Df = last_100_days.append(data_testing , ignore_index=True)
-input_data = scaler.fit_transform(final_Df)
+final_data = last_100_days.append(data_testing , ignore_index=True)
+input_data = scaler.fit_transform(final_data)
 
 x_test = []
 y_test = []
